d47e38661bb91fe051d8aa98422482f08efed7aa
shahidafridi <work.shahid.afridi@gmail.com>
2024-11-25 18:53:34 +0530

    Deployment related

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
+const nextConfig = {
+  basePath: process.env.NODE_ENV !== "production" ? undefined : "/Portfolio",
+  assetPrefix: process.env.NODE_ENV !== "production" ? undefined : "/Portfolio/",
+  output: process.env.NODE_ENV !== "production" ? undefined : "export",
+  images: {
+    unoptimized: envImageUnoptimize,
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
