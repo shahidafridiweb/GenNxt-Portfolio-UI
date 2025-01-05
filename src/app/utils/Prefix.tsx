14a3db1c12451e1beca6a034d6a0a2601db50cc5
Shahid Afridi <work.shahid.afridi@gmail.com>
2025-01-05 08:53:09 +0530

    Updating Visit Tracker

--- src/app/utils/Prefix.tsx
+++ src/app/utils/Prefix.tsx
@@ -1,4 +1,4 @@
 const prefix = process.env.NODE_ENV !== "production" ? "" : "/GenNxt-Portfolio-UI";
-const ghToken = process.env.NEXT_PUBLIC_GH_TOKEN='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';
+const ghToken = process.env.NEXT_PUBLIC_GH_TOKEN='';
 
 export { prefix , ghToken};
--- src/app/utils/VisitTracker.tsx
+++ src/app/utils/VisitTracker.tsx
@@ -3,34 +3,9 @@ import { useEffect } from 'react';
 import { ghToken } from './Prefix';
 
 const VisitTracker = () => {
-//   useEffect(() => {
-//     fetch('https://api.github.com/repos/shahidafridiweb/GenNxt-Portfolio-UI/dispatches', {
-//       method: 'POST',
-//       headers: {
-//         'Authorization': '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',
-//         'Accept': 'application/vnd.github.v3+json'
-//       },
-//       body: JSON.stringify({
-//         event_type: 'visit'
-//       })
-//     });
-//   }, []);
-  useEffect(() => { 
-    fetch('https://api.github.com/repos/shahidafridiweb/GenNxt-Portfolio-UI/dispatches', 
-    { 
-        method: 'POST', 
-        headers: { 
-            'Authorization': `Bearer ${ghToken}`, 
-            'Accept': 'application/vnd.github.v3+json' 
-        }, 
-            
-        body: JSON.stringify({ event_type: 'visit' }) }) 
-        .then(response => { 
-            if (!response.ok) 
-            { throw new Error(`HTTP error! status: ${response.status}`); } return response.json(); }) 
-            .then(data => { console.log('Dispatch event triggered:', data); }) 
-            .catch(error => { console.error('Error triggering dispatch event:', error); }); 
-        }, []);
+    useEffect(() => { const timestamp = new Date().toISOString(); fetch('https://api.github.com/repos/<your-username>/<your-repo>/dispatches', { method: 'POST', headers: { 'Authorization': `Bearer ${ghToken}`, 'Accept': 'application/vnd.github.v3+json' }, body: JSON.stringify({ event_type: 'visit', client_payload: { timestamp: timestamp } }) }) .then(response => { if (!response.ok) { throw new Error(`HTTP error! status: ${response.status}`); } return response.json(); }) .then(data => { console.log('Dispatch event triggered:', data); }) .catch(error => { console.error('Error triggering dispatch event:', error); }); }, []);
+    
+    
 
   return null;
 };
