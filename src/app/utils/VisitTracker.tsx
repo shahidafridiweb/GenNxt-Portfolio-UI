a500674b1f445dc27a10f422a804e7f4534ac90c
Shahid Afridi <work.shahid.afridi@gmail.com>
2025-01-05 07:32:12 +0530

    Adding Site visit tracker

--- src/app/utils/VisitTracker.tsx
+++ src/app/utils/VisitTracker.tsx
@@ -2,18 +2,34 @@
 import { useEffect } from 'react';
 
 const VisitTracker = () => {
-  useEffect(() => {
-    fetch('https://api.github.com/repos/shahidafridiweb/GenNxt-Portfolio-UI/dispatches', {
-      method: 'POST',
-      headers: {
-        'Authorization': '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',
-        'Accept': 'application/vnd.github.v3+json'
-      },
-      body: JSON.stringify({
-        event_type: 'visit'
-      })
-    });
-  }, []);
+//   useEffect(() => {
+//     fetch('https://api.github.com/repos/shahidafridiweb/GenNxt-Portfolio-UI/dispatches', {
+//       method: 'POST',
+//       headers: {
+//         'Authorization': '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',
+//         'Accept': 'application/vnd.github.v3+json'
+//       },
+//       body: JSON.stringify({
+//         event_type: 'visit'
+//       })
+//     });
+//   }, []);
+  useEffect(() => { 
+    fetch('https://api.github.com/repos/shahidafridiweb/GenNxt-Portfolio-UI/dispatches', 
+    { 
+        method: 'POST', 
+        headers: { 
+            'Authorization': `Bearer ${process.env.NEXT_PUBLIC_GH_TOKEN}`, 
+            'Accept': 'application/vnd.github.v3+json' 
+        }, 
+            
+        body: JSON.stringify({ event_type: 'visit' }) }) 
+        .then(response => { 
+            if (!response.ok) 
+            { throw new Error(`HTTP error! status: ${response.status}`); } return response.json(); }) 
+            .then(data => { console.log('Dispatch event triggered:', data); }) 
+            .catch(error => { console.error('Error triggering dispatch event:', error); }); 
+        }, []);
 
   console.log('Visit tracked');
 
